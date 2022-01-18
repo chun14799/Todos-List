2da1a1774194e2612c64501807282a5f15d9441e
Hoang Long <danghuynhhoanglong@gmail.com>
2022-01-18 18:41:43 +0700

    update new UI

--- src/App.js
+++ src/App.js
@@ -5,34 +5,13 @@ import { useState } from 'react';
 
 //functional component
 const App = () => {
-<<<<<<< HEAD
 
   const [todos, setTodos] = useState([])
   const [input, setInput] = useState("")
   const [selectedTodo, setSelectedTodo] = useState(null)
   const [clicked, setClicked] = useState(false)
-  const [pending, setPending] = useState(todos.length)
 
-=======
-  //state
-  //react hooks
-  const [input, setInput] = useState(""); //[a,b]
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      content: "Todo 1",
-      isDone: false,
-    },
-    {
-      id: 2,
-      content: "Todo 2",
-      isDone: false,
-    },
-  ]);
-  const [selectedTodos, setSelectedTodos] = useState(null)
-  const [click, setClick] = useState(false)
-  // 1 state ---> tên (age), function để thay đổi state đó()
->>>>>>> 20006d355f6f70e5e1ed3f90b7c280df4e76ced6
+
   const handleChange = (e) => {
     setInput(e.target.value)
   };
@@ -40,7 +19,6 @@ const App = () => {
 
 
   const handleSubmit = (e) => {
-<<<<<<< HEAD
     e.preventDefault()
     if (!selectedTodo) {
       setTodos([
@@ -52,14 +30,6 @@ const App = () => {
         }
       ])
       setInput("")
-      setPending(todos.length + 1)
-=======
-    e.preventDefault();
-    if (!selectedTodos) {
-      //add
-      setTodos([...todos, { id: Date.now(), content: input, isDone: false }])
-      setInput("");
->>>>>>> 20006d355f6f70e5e1ed3f90b7c280df4e76ced6
     } else {
       const newTodos = todos.map((val) => {
         if (val.id === selectedTodo.id) {
@@ -74,15 +44,6 @@ const App = () => {
       setInput("")
       setSelectedTodo(null)
     }
-<<<<<<< HEAD
-=======
-
-  };
-
-  const handleDelete = (id) => {
-    const newTodos = todos.filter(val => val.id !== id)
-    setTodos(newTodos)
->>>>>>> 20006d355f6f70e5e1ed3f90b7c280df4e76ced6
   }
 
   const handleUpdate = (id) => {
@@ -95,7 +56,6 @@ const App = () => {
     setInput("")
   }
 
-<<<<<<< HEAD
   const handleDelete = (id) => {
     const newTodos = todos.filter(val => val.id !== id)
     setTodos(newTodos)
@@ -177,76 +137,6 @@ const App = () => {
               <button className='check_all' onClick={handleCheckAll}>{clicked ? "Uncheck All" : "Check All"}</button>
             </div>
           </div>
-=======
-  const handleCheck = (id) => {
-    setTodos(
-      todos.map((val)=>{
-        if(val.id === id){
-          return{
-            ...val,
-            isDone: !val.isDone,
-          };
-        }
-        return val;
-      })
-    )
-    console.log(todos)
-  }
-
-  const handleDeleteAll = () =>{
-    setTodos([])
-  }
-
-  const handleCheckAll = () =>{
-    setClick(false)
-    setTodos(
-      todos.map((val)=>{
-        return{
-          ...val,
-            isDone: !val.isDone,
-          }
-      })
-    )
-    if(click === true){
-      setClick(false)
-    }else{
-      setClick(true)
-    }
-  }
-//  let pendingNumber = document.querySelector(".pendingNumber");
-//   pendingNumber.textContent = todos.length;
-
-  return (
-    <div className="App">
-      <div className="todo">
-        <h2>TODO LIST</h2>
-        <form onSubmit={handleSubmit}>
-          <input value={input} type="text" onChange={handleChange} />
-          <button>{selectedTodos ? "Update" : "Add"}</button>
-          {selectedTodos && <button onClick={handleCancle}>Cancle</button>}
-
-        </form>
-        <ul>
-          {todos.length !== 0 ? 
-          (todos.map((val) => (
-            <li key={val.id} className={ val.isDone === true  ? "checked" : ""}>
-              {val.content}
-
-              <div className="icon">
-                <button onClick={() => handleCheck(val.id)}>Done</button>
-                <i onClick={() => handleUpdate(val.id)} className="fas fa-edit"></i>
-                <i onClick={() => handleDelete(val.id)} className="fas fa-trash"></i>
-              </div>
-            </li>
-          ))) : (
-            <li>No Item </li>
-          )}
-        </ul>
-        {/* <span>Bạn đang có <span className="pendingNumber"></span> task</span> */}
-        <div className="bottom_btn">
-          <button className="checkAll" onClick={handleCheckAll}>{click === true ? "Unchecked All" : "Check All"}</button>
-          <button onClick={handleDeleteAll}>Delete All</button>
->>>>>>> 20006d355f6f70e5e1ed3f90b7c280df4e76ced6
         </div>
       </div>
     </div >
